c6dee38cb30c83faa9e79d4d1cd371078f7eef2b
Paul <phleguy@yahoo.fr>
2017-03-03 18:31:40 +0100

    form post/edit modified

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-  	params.require(:post).permit(:title, :content, :slug, :image)
+  	params.require(:post).permit(:title, :content, :slug, :image, :category_id)
   end
 
   def set_post
